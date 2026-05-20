445c70859714c95cc36f103f3c6ff11250d08deb
tano715 <takashirotano715@gmail.com>
2026-05-20 17:03:22 +0900

    JSでレスポンスを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -32,7 +32,6 @@ public class PostController {
     post.setContent(form.getContent());
     postRepository.insert(post);
     PostEntity resultPost = postRepository.findById(post.getId());
-    System.out.println(resultPost);
     return ResponseEntity.ok(resultPost);
   }
   
